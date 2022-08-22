f9cde2702d04393a0d327ec94be6fccf3aa76098
famarech <marechalfabrice@hotmail.fr>
2022-08-22 18:23:41 +0200

    etablissement d'un dictionnaire

--- main.py
+++ main.py
@@ -20,31 +20,40 @@ root = tree.getroot()
 
 RANK = 0
 MARKS = []
+CSV = []
 
 def level(root, count, mark):
     global RANK
     global MARKS
     count += 1
     number = 0
+
     mark = mark + ' ' + root.tag
     for r in root:
         number += 1
         if r.tag != '':
+            temp = {}
+            temp[mark + ' ' + r.tag] = r.text
+            CSV.append([temp, number])
             if count == RANK:
                 MARKS.append(mark + ' ' + r.tag)
             if count > RANK:
                 RANK += 1
             level(r, count, mark)
+
     return number
 
 n = level(root, 0, '')
 l = erase_duplicate(MARKS)
-# for each in l:
-#     print(each)
 
-datas = [l]
-for i in range(n):
-    datas.append([0]*len(l))
-print(datas)
+for each in CSV:
+    print(each)
+
+##datas = [l]
+##for i in range(n):
+##    datas.append([0]*len(l))
+##print(datas)
 
 # ne reste plus qu'à ranger les informations au bon endroit dans le tableau
+
+
